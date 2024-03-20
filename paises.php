8ca0d89a4859f6353662e07c544202026a586dba
morceli <109186798+joaotds@users.noreply.github.com>
2024-03-20 17:00:57 -0300

    Update paises.php

--- paises.php
+++ paises.php
@@ -1,7 +1,8 @@
 <?php
 
 function desenhaLinha(array $linha) {
-
+    // Exibindo os valores da linha formatados
+    echo "{$linha['ordem']}. {$linha['pais']} - Ouro: {$linha['ouro']}, Prata: {$linha['prata']}, Bronze: {$linha['bronze']}, Total: {$linha['total']}\n";
 }
 
 $linha1 = array("ordem" => 1, "pais" => "Estados Unidos", "ouro" => 46, "prata" => 37, "bronze" => 38,  "total" => 121);
@@ -10,7 +11,11 @@ $linha3 = array("ordem" => 3, "pais" => "China", "ouro" => 26, "prata" => 18, "b
 $linha4 = array("ordem" => 4, "pais" => "Russia", "ouro" => 19, "prata" => 17, "bronze" => 20,  "total" => 56);
 $linha5 = array("ordem" => 5, "pais" => "Alemanha", "ouro" => 17, "prata" => 10, "bronze" => 15,  "total" => 42);
 
-//criar a matriz
-$linhas = array($linha1,$linha2,$linha3,$linha4,$linha5);
+// Criar a matriz
+$linhas = array($linha1, $linha2, $linha3, $linha4, $linha5);
 
-//C Percorrer a matriz para gerar
+// Percorrer a matriz e chamar a função para desenhar a linha
+foreach ($linhas as $linha) {
+    desenhaLinha($linha);
+}
+?>
